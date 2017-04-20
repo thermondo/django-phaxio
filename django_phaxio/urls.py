c57a7afedafffc2a483032ecb674ba1a5f5bb1c7
Maik Hoepfel <m@maikhoepfel.de>
2017-04-20 13:56:10 +0200

    Include application namespace for Django 2 support
    The Django master tests failed because Django 2.0 requires setting the
    app name space. It seems that the usual way to do it seems to specify it
    in the urls.py that gets included.
    
    https://docs.djangoproject.com/en/dev/topics/http/urls/#url-namespaces-and-included-urlconfs

--- django_phaxio/urls.py
+++ django_phaxio/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+# Set application namespace (Django 2.0+)
+# https://docs.djangoproject.com/en/dev/topics/http/urls/#url-namespaces-and-included-urlconfs
+app_name = 'django_phaxio'
+
 urlpatterns = [
     url(r'^callback$',
         views.PhaxioCallbackView.as_view(),
